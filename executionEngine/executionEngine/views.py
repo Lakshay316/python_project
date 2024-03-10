17a0629a3deb6684be0b931261fe9e3b0ca3631f
Lakshay <imlakshay@gmail.com>
2024-03-10 23:54:01 +0530

    fix: first page creation

--- executionEngine/executionEngine/views.py
+++ executionEngine/executionEngine/views.py
@@ -5,18 +5,71 @@ def index(request):
     return render(request, 'index.html', {'title': 'Home'})
 
 
-def index1(request):
-    return render(request, 'page1.html', {'title': 'Page 1', 'userName': 'Harsh'})
+def screen1(request):
+    response = {
+        'title': 'Consolidation & Extrapolation',
+        'grids': [
+            {
+                'title': 'Input / Output Current Path',
+                'inputFields': [
+                    {
+                        'inputName': 'Current T-Zero',
+                        'inputType': 'text',
+                    },
+                    {
+                        'inputName': 'Previous T-Zero',
+                        'inputType': 'text',
+                    },
+                    {
+                        'inputName': 'NTMR',
+                        'inputType': 'text',
+                    },
+                    {
+                        'inputName': 'Saracen',
+                        'inputType': 'text',
+                    },
+                    {
+                        'inputName': 'Reference',
+                        'inputType': 'text',
+                    },
+                    {
+                        'inputName': 'Output',
+                        'inputType': 'text',
+                    },
+                    # {
+                    #     'inputName': 'Gender',
+                    #     'inputType': 'dropDown',
+                    #     'default': 'Male',
+                    #     'dropDownOptions': ['Male', 'Female', 'Others']
+                    # }
+                ]
+            },
+            {
+                'title': 'NTMR Reconciliation Threshold',
+                'inputFields': [
+                    {
+                        'inputName': 'Absolute',
+                        'inputType': 'number',
+                    },
+                    {
+                        'inputName': 'Percentage',
+                        'inputType': 'float',
+                    }
+                ]
+            }
+        ]
+    }
+    return render(request, 'screen1.html', response)
 
 
-def index2(request):
-    return render(request, 'page2.html', {'title': 'Page 2'})
+def screen2(request):
+    return render(request, 'screen2.html', {'title': 'Page 2'})
 
 
-def index3(request):
-    return render(request, 'page3.html', {'title': 'Page 3'})
+def screen3(request):
+    return render(request, 'screen3.html', {'title': 'Page 3'})
 
 
-def index4(request):
-    return render(request, 'page4.html', {'title': 'Page 4'})
+def screen4(request):
+    return render(request, 'screen4.html', {'title': 'Page 4'})
 
